e7e987f54a2bfe364aeb18f16bc4b8dddad7db42
alexklavens <aklavens@conncoll.edu>
2018-02-24 18:02:38 -0500

    File Upload
    Function csvLower(__readFromCSV, __writeToCSV = "") returs filename of rewriten lowercased __readFromCSV file

---
+++ csvLower.py
@@ -0,0 +1,28 @@
+##lowercase all fields in csv file
+
+import csv
+def csvLower(__readFromCSV, __writeToCSV = ""):
+    """__readFromCSV takes "<filename>.csv"
+       __writeToCSV optionally takes a write file.
+       Defaults to filename created based on __readFromCSV name
+
+       RETURNS __writeToCSV filename"""
+
+       
+    if __writeToCSV == "":
+        __writeToCSV = __readFromCSV[:-4] + "_csvLower" + __readFromCSV[-4:]
+
+
+    with open(__readFromCSV, newline='',encoding = "utf-8") as in_file:
+        with open(__writeToCSV, 'w', newline='',encoding="utf-8") as out_file:
+            reader = csv.reader(in_file, delimiter=',', quotechar='|')
+            csvWrite = csv.writer(out_file, delimiter=',', quotechar='|')
+
+            for row in reader:
+                newRow = []
+                for item in row:
+                    newRow.append(item.lower())
+
+                csvWrite.writerow(newRow)
+
+    return __writeToCSV
